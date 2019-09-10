5618b88f30921c1f4e0f390cf4a3695714e423de
Matt Santos <mattsantos541@gmail.com>
2019-09-10 16:50:40 -0500

    Total_spent

--- 4.1_python_introduction_exercises.py
+++ 4.1_python_introduction_exercises.py
@@ -53,3 +53,23 @@ True or "42"
 [1] * [2]= error
 [] + [] == []= True
 {} + {}= error
+
+
+#1 You have rented some movies for your kids: The 
+# little mermaid (for 3 days), Brother Bear (for 
+# 5 days, they love it), and Hercules (1 day, you don't 
+# know yet if they're going to like it). If price for a 
+# movie per day is 3 dollars, how much will you have to pay?
+
+Movies= [
+
+{"Movie":"Little Mermaid", "Rental": 3, "preference":"Do no know if they like it"},
+{"Movie":"Brother Bear", "Rental": 5, "preference":"Kids Love it"},
+{"Movie":"Herceules", "Rental": 1, "preference":"Do not know if they like it"}
+]
+
+##If price for a movie per day is 3 dollars, 
+# how much will you have to pay?
+Total_Spent= 0  
+for x in Movies: 
+    Total_Spent = x["Rental"] * 3
