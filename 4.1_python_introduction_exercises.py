3941426c6ed2abad3903a4dcbc27896eddf5af8a
Matt Santos <mattsantos541@gmail.com>
2019-09-10 16:10:38 -0500

    Firwt Exercises #2

--- 4.1_python_introduction_exercises.py
+++ 4.1_python_introduction_exercises.py
@@ -26,3 +26,30 @@ The price of a product? float
 A Matrix? dict
 The email addresses collected from a registration form? tuple
 Information about applicants to Codeup's data science program? str
+
+##For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in your Python REPL.
+
+'1' + 2= Error
+6 % 4= 2
+type(6 % 4)= int
+type(type(6 % 4))= type
+'3 + 4 is ' + 3 + 4= error
+0 < 0= False
+'False' == False= False
+True == 'True'= False
+5 >= -5= True
+!False or False
+True or "42"
+!True && !False
+6 % 5 = 1
+5 < 4 and 1 == 1 = False
+'codeup' == 'codeup' and 'codeup' == 'Codeup'= False
+4 >= 0 and 1 !== '1'= Error
+6 % 3 == 0 True
+5 % 2 != 0 True
+[1] + 2= Error
+[1] + [2]= [1, 2]
+[1] * 2= [1, 1]
+[1] * [2]= error
+[] + [] == []= True
+{} + {}= error
