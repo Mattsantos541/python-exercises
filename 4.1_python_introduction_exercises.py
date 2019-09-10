8ecd093f276d2997761c8c83d538cd4bbc2fa07c
Matt Santos <mattsantos541@gmail.com>
2019-09-10 16:00:35 -0500

    exercises #1

--- 4.1_python_introduction_exercises.py
+++ 4.1_python_introduction_exercises.py
@@ -4,3 +4,25 @@ print ("Hello World")
 greetings= "How are you?"
 print (greetings)
 ####this is working
+
+
+###first exercises
+type(99.9)
+type("False")
+type(False)
+type('0')
+type(0)
+type(True)
+type('True')
+type([{}])
+type({'a': []})
+
+A term or phrase typed into a search box? string
+If a user is logged in? bool
+A discount amount to apply to a user's shopping cart? float
+Whether or not a coupon code is valid? bool
+An email address typed into a registration form? tuple
+The price of a product? float
+A Matrix? dict
+The email addresses collected from a registration form? tuple
+Information about applicants to Codeup's data science program? str
