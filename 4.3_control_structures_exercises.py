dac1a276bc7908747ef91033c3cbe63c8bb694cc
Matt Santos <mattsantos541@gmail.com>
2019-09-11 15:26:07 -0500

    #3

--- 4.3_control_structures_exercises.py
+++ 4.3_control_structures_exercises.py
@@ -1,3 +1,27 @@
 #control exercises
 
-print ("Hello World")
+#Conditional Basics
+
+#1a
+##prompt the user for a day of the week, print out whether the day is Monday or not
+
+day = input("day of the week")
+if day == "Monday":
+    print("Monday")
+else:
+    print ("not Monday")
+#b
+###prompt the user for a day of the week, print out whether the day is Monday or not
+day = input("day of the week")
+if day == ("Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday":
+    print ("Weekday")
+else:
+    print ("not workday")
+#c 
+##create variables and make up values for
+hours= 40
+rate= 25
+if hours <= 40:
+    total = hours * rate
+else:
+    total = 40 * rate + (hours - 40) * (1.5 * rate)
