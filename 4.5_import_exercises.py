8f687cddaed3392402e097407affdae149ff129d
Matt Santos <mattsantos541@gmail.com>
2019-09-16 10:36:59 -0500

    combinations

--- 4.5_import_exercises.py
+++ 4.5_import_exercises.py
@@ -1,4 +1,20 @@
-import functions_exercises as funcE
-funcE.apply_discount
-funcE.calculate_tip sas Functip
+import functions_exercises
 
+functions_exercises.remove_vowels()
+
+
+from functions_exercises import is_two()
+
+FuE.is_two()
+
+#How many different ways can you combine the letters from "abc" with the numbers 1, 2, and 3?
+
+list_1= ["a, b, c"]
+list_2= [1, 2, 3]
+from itertools import combinations(list_1, list_2), 2)
+
+#How many different ways can you combine two of the letters from "abcd"?
+
+from itertools import combinations
+list= ['a', 'b', 'c', 'd']
+print(combinations(list), 2)
