11a2c84fda4f2b7aafd8882a391f4a19fd57f5d7
Matt Santos <mattsantos541@gmail.com>
2019-09-16 16:59:51 -0500

    sexond commit

--- Command_line_checkbook.py
+++ Command_line_checkbook.py
@@ -1,1 +1,15 @@
 ##Command Line Checkbook Application
+
+##Greet on opening
+name= input( "Welcome, may i get your name? ")
+print('Hello ' + str(name) +' What can I do for you? ' )
+def checking_options():
+  print ("What can I do for you today? ")
+  print([
+    "1. Balance",
+    "2. Deposit",
+    "3. Withdrawl",
+    "4. End Session"
+  ])
+
+checking_options()
