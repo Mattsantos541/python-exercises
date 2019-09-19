6a1a38b5271c12419f7afa1e5edc714cae2633e4
Matt Santos <mattsantos541@gmail.com>
2019-09-19 14:05:28 -0500

    #1 plot this equation

---
+++ 4.6.1_matplotlib_exercises.py
@@ -0,0 +1,23 @@
+###mathplotlib exercises
+
+#Use matplotlib to plot the following equation: y = x **2 -x +2
+
+
+
+#Add an anotation for the point 0, 0, the origin.
+
+
+######
+
+#Create and label 4 separate charts for the following equations (choose a range for x that makes sense):
+
+
+###
+#Combine the figures you created in the last step into one large figure with 4 subplots.
+
+
+####
+
+#Comine the figures you created in the last step into one figure where each of the 4 equations has a different color for the points. Be sure to include a legend.
+
+
