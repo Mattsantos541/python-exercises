4837f8038020126ff960d70dff827da7ff94c85a
Matt Santos <mattsantos541@gmail.com>
2019-09-26 11:11:36 -0500

    #3

--- panda_DF.py
+++ panda_DF.py
@@ -23,6 +23,7 @@ df = pd.DataFrame({'name': students,
 #Create a column named passing_english that indicates whether each student has a passing grade in reading.
 df['passing_english'] = df.english > 60
 #Sort the english grades by the passing_english column. How are duplicates handled?
+
 #Sort the english grades first by passing_english and then by student name. All the students that are failing english should be first, and within the students that are failing english they should be ordered alphabetically. The same should be true for the students passing english. (Hint: you can pass a list to the .sort_values method)
 df.sort_values(by=['name'])
 #Sort the english grades first by passing_english, and then by the actual english grade, similar to how we did in the last step.
@@ -36,3 +37,33 @@ df.sort_values(by=['name'])
 mpg.info()
 mpg.describe()
 #What are the data types of each column?
+results= mpg.dtypes
+print(results)
+
+#Rename the cty column to city.
+mpg.rename(columns={'cty': 'city'})
+#Rename the hwy column to highway.
+mpg.rename(columns={'hwy': 'highway'})
+
+#Do any cars have better city mileage than highway mileage?
+
+Create a column named mileage_difference this column should contain the difference between highway and city mileage for each car.
+Which car (or cars) has the highest mileage difference?
+Which compact class car has the lowest highway mileage? The best?
+Create a column named average_mileage that is the mean of the city and highway mileage.
+Which dodge car has the best average mileage? The worst?
+
+# 3 Load the Mammals dataset. Read the documentation for it, and use the data to answer these questions:
+
+#How many rows and columns are there?
+
+#What are the data types?
+result = Mammals.dtypes 
+print (result)
+#Summarize the dataframe with .info and .describe
+Mammals.info()
+Mammals.describe
+#What is the the weight of the fastest animal?
+Mammals.sort_values(by= "speed")
+#What is the overal percentage of specials?
+#How many animals are hoppers that are above the median speed? What percentage is this?
