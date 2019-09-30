e91819aece411ab156c48bdd30e84e114c5d9e3b
Matt Santos <mattsantos541@gmail.com>
2019-09-30 11:53:42 -0500

    iris #2

--- 4.9_seaborn_exercises.py
+++ 4.9_seaborn_exercises.py
@@ -3,3 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import seaborn as sns
+
+from pydataset import data
+
+
+#1 What does the distribution of petal lengths look like?
+
+sns.distplot(iris.petal_length)
+
+#2 s there a correlation between petal length and petal width?
+sns.relplot(data=iris, x='petal_length', y='petal_width')
